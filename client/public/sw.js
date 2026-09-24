4da4dd03edb30aea55d8871a373419a54a632eba
wealthforgerbusiness-source <wealthforgerbusiness@gmail.com>
2026-09-24 08:39:56 +0100

    Add service worker for file caching
    Implement service worker for caching files.

---
+++ client/public/sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'bana-frites-cache-v1';
+
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/logo-192.png',
+  '/logo-512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
